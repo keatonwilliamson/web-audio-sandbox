6ec0a3719f5884a94f7f13faac5506df79604983
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-29 18:43:40 -0600

    oscillator connected

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import QwertyHancock from './QwertyHancock'
 function App() {
   useEffect(() => {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    var context = new AudioContext(),
-      settings = {
+    var context = new AudioContext();
+    let keyboard = new QwertyHancock({
         id: 'keyboard',
         width: 600,
         height: 150,
@@ -17,14 +17,17 @@ function App() {
         borderColour: '#000',
         activeColour: 'yellow',
         octaves: 2
-      },
-      keyboard = new QwertyHancock(settings);
+      });
 
     let masterGain = context.createGain();
-    let nodes = [];
+    let analyser = context.createAnalyser();
+
+
 
     masterGain.gain.value = 0.3;
-    masterGain.connect(context.destination);
+    masterGain.connect(analyser).connect(context.destination);
+
+    let nodes = [];
 
     keyboard.keyDown = function (note, frequency) {
       var oscillator = context.createOscillator();
@@ -51,19 +54,69 @@ function App() {
       nodes = new_nodes;
     };
 
-    // Analytics
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-32368229-1']);
-    _gaq.push(['_trackPageview']);
+    // // Analytics
+    // var _gaq = _gaq || [];
+    // _gaq.push(['_setAccount', 'UA-32368229-1']);
+    // _gaq.push(['_trackPageview']);
 
-    (function () {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    // (function () {
+    //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    // })();
+
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+
+    var canvas = document.querySelector('.visualizer');
+    var canvasCtx = canvas.getContext("2d");
+    let WIDTH = canvas.width;
+    let HEIGHT = canvas.height;
+    // draw an oscilloscope of the current audio source
+
+    function draw() {
+
+      requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+      canvasCtx.beginPath();
+
+      var sliceWidth = WIDTH * 1.0 / bufferLength;
+      var x = 0;
+
+      for (var i = 0; i < bufferLength; i++) {
+
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+    };
+
+    draw();
+
   });
   return (
     <div className="App">
+      <canvas class="visualizer" width="375" height="100"></canvas>
       <div id="keyboard"></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
